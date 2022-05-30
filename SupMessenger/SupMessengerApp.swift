5aa6d1404b9eaa3797d36e91debbf91859e0a548
Yaroslav <toasolek@gmail.com>
2022-05-30 12:34:12 +0700

    Initial Commit

---
+++ SupMessenger/SupMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupMessengerApp.swift
+//  SupMessenger
+//
+//  Created by Yaroslav Derbyshev on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SupMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
